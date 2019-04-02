05797b3b70618cc4d1f65527eec886273e3b2d39
Alexander Brown <systemdevcloud@live.co.uk>
2019-04-02 17:42:41 +0100

    move css to the template file and remove from the .info also add a health.css file

--- template.php
+++ template.php
@@ -9,4 +9,23 @@ function simpletheme_preprocess_page(&$variables) {
 	// move from the page.tpl.php and place it in the template.php file as a preprocess
 
 	$variables['simpletheme_subheading'] = theme_get_setting('simpletheme_subheading');
+
+	
+
+
+
+
+if($variables['node']->type == 'article') {
+	
+	drupal_add_css(drupal_get_path("theme", "simpletheme"). '/css/style.css');
+
+	
 }
+
+
+
+}
+
+
+
+// check to see if its a node based page
