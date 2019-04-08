742f7cb2302535f7a7174ae46be087e8f48b91d1
Alexander Brown <systemdevcloud@live.co.uk>
2019-04-08 22:58:00 +0100

    add a submit button

--- template/form/donate.tpl.php
+++ template/form/donate.tpl.php
@@ -1,20 +1,24 @@
 <div class="container">
 
 <div id="first_name">
+<label>First Name
 <?php print render($form['first_name']); ?>
 </div>
 
+
 <div id="last_name">
+<label>Last Name
 <?php print render($form['last_name']); ?>
 </div>
 
 <div id="address">
+<label>Address
 <?php print render($form['address']); ?>
 </div>
 
+<div id="submit">
+	<?php print render($form['submit']); ?>
 </div>
-
-
 
 
 
